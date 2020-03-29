3a018b38dabb7efad343982e6cf9c67a6fd8d8c4
hsn6 <hsn6@tuta.io>
2020-03-29 18:31:15 +0430

    Add auth forms

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Authentication from './components/Authentication'
+import Authentication from './components/pages/Authentication'
 
 function Routes() {
   return (
--- src/components/pages/Authentication/index.js
+++ src/components/pages/Authentication/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import './index.css'
-import { Typography, Button, Card } from '@material-ui/core'
+import { Typography, Button, Card, TextField } from '@material-ui/core'
 
 function slide_class(index, slideFadeIn, slideFadeOut, slideDisplayFlex) {
   return `slide ${slideFadeIn === index ? 'fade-in' : ''} ${
@@ -9,9 +9,12 @@ function slide_class(index, slideFadeIn, slideFadeOut, slideDisplayFlex) {
 }
 
 function Authentication() {
-  const [slideFadeOut, setSlideFadeOut] = useState(1)
-  const [slideFadeIn, setSlideFadeIn] = useState(0)
-  const [slideDisplayFlex, setSlideDisplayFlex] = useState(0)
+  const [slideFadeOut, setSlideFadeOut] = useState(0)
+  const [slideFadeIn, setSlideFadeIn] = useState(1)
+  const [slideDisplayFlex, setSlideDisplayFlex] = useState(1)
+  // const [slideFadeOut, setSlideFadeOut] = useState(1)
+  // const [slideFadeIn, setSlideFadeIn] = useState(0)
+  // const [slideDisplayFlex, setSlideDisplayFlex] = useState(0)
   useEffect(() => {}, [])
   return (
     <Card id="center-card">
@@ -45,10 +48,14 @@ function Authentication() {
         id="slide_1"
         className={slide_class(1, slideFadeIn, slideFadeOut, slideDisplayFlex)}
       >
-        <Typography variant="h1">oohoom</Typography>
-        <Typography variant="h4" align="center">
-          Login
+        <Typography variant="h2" align="center">
+          mobile verification
         </Typography>
+        <TextField
+          label="mobile"
+          helperText="ex: 00989389742591"
+          style={{ marginTop: '5rem' }}
+        />
         <Button
           variant="outlined"
           onClick={e => {
@@ -64,33 +71,59 @@ function Authentication() {
           size="large"
           style={{ marginTop: '10rem' }}
         >
-          Login
+          Send me a sms
         </Button>
       </div>
       <div
         id="slide_2"
         className={slide_class(2, slideFadeIn, slideFadeOut, slideDisplayFlex)}
       >
-        <Typography variant="h1">oohoom</Typography>
-        <Typography variant="h4" align="center">
-          Regsiter
-        </Typography>
+        <Typography variant="h2">Login</Typography>
+        <TextField
+          label="code"
+          helperText="code you recieved"
+          style={{ marginTop: '5rem' }}
+        />
         <Button
           variant="outlined"
           onClick={e => {
             setSlideFadeOut(2)
             setSlideFadeIn(-1)
             setTimeout(() => {
-              setSlideDisplayFlex(0)
+              setSlideDisplayFlex(3)
             }, 1000)
             setTimeout(() => {
-              setSlideFadeIn(0)
+              setSlideFadeIn(3)
             }, 1300)
           }}
           size="large"
           style={{ marginTop: '10rem' }}
         >
-          Regsiter
+          Login
+        </Button>
+      </div>
+      <div
+        id="slide_3"
+        className={slide_class(3, slideFadeIn, slideFadeOut, slideDisplayFlex)}
+      >
+        <Typography variant="h2">Register</Typography>
+        <TextField
+          label="code"
+          helperText="code you recieved"
+          style={{ marginTop: '5rem' }}
+        />
+        <TextField
+          label="name"
+          helperText="a unique name"
+          style={{ marginTop: '2rem' }}
+        />
+        <Button
+          variant="outlined"
+          onClick={e => {}}
+          size="large"
+          style={{ marginTop: '10rem' }}
+        >
+          Register
         </Button>
       </div>
     </Card>
